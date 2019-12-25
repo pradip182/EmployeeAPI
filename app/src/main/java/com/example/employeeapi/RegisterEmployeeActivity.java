b8b6c96a532725d3b3a1be77b08a703432778aec
pradip <pradipacharya182@gmail.com>
2019-12-25 10:56:47 +0545

    Register vako cha

--- app/src/main/java/com/example/employeeapi/RegisterEmployeeActivity.java
+++ app/src/main/java/com/example/employeeapi/RegisterEmployeeActivity.java
@@ -3,12 +3,76 @@ package com.example.employeeapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.employeeapi.api.EmployeeApi;
+import com.example.employeeapi.url.EmployeeCUD;
+import com.example.employeeapi.url.Url;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterEmployeeActivity extends AppCompatActivity {
+
+    private EditText etName,etSalary,etEmpAge;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_employee);
+
+        etName = findViewById(R.id.etName);
+        etSalary = findViewById(R.id.etSalary);
+        etEmpAge = findViewById(R.id.etEmpAge);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
     }
+
+    private void Register(){
+
+        String name = etName.getText().toString();
+        Float salary = Float.parseFloat(etSalary.getText().toString());
+        int age = Integer.parseInt(etEmpAge.getText().toString());
+
+        EmployeeCUD employee =  new EmployeeCUD(name,salary,age);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Url.base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        EmployeeApi employeeAPI = retrofit.create(EmployeeApi.class);
+        Call<Void> voidCall = employeeAPI.registerEmployee(employee);
+
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(RegisterEmployeeActivity.this, "You have been successfully registered", Toast.LENGTH_SHORT).show();
+            }
+
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(RegisterEmployeeActivity.this, "Error :"+ t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/employeeapi/api/EmployeeApi.java
+++ app/src/main/java/com/example/employeeapi/api/EmployeeApi.java
@@ -1,11 +1,16 @@
 package com.example.employeeapi.api;
 
 import com.example.employeeapi.model.Employee;
+import com.example.employeeapi.url.EmployeeCUD;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface EmployeeApi {
@@ -13,6 +18,16 @@ public interface EmployeeApi {
     @GET("employees")
     Call<List<Employee>> getEmployee();
 
+    @POST("create")
+    Call<Void> registerEmployee (@Body EmployeeCUD emp);
+
+
     @GET("employee/{empID}")
     Call<Employee> getEmployeeByID(@Path("empID") int empId);
+
+    @PUT("update/{empID}")
+    Call<Void>updateEmployee(@Path("empID") int empId, @Body EmployeeCUD emp);
+
+    @DELETE("delete/{empID")
+    Call<Void>deleteEmployee(@Path("empID") int empId);
 }
